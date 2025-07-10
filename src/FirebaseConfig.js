87e5218be29d05257abe7a9baf4a95211f4ddc16
João Machado <joaoguilhermemachado1@gmail.com>
2025-07-10 16:31:28 +0100

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIuTPfdZDhNxXMXXnp5u7NhQFrj4MiGSU",
-  authDomain: "projetofinal-f4f26.firebaseapp.com",
-  projectId: "projetofinal-f4f26",
-  storageBucket: "projetofinal-f4f26.firebasestorage.app",
-  messagingSenderId: "437784945984",
-  appId: "1:437784945984:web:43dd78ffeb1b669a6006a5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -19,3 +19,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export default app;
